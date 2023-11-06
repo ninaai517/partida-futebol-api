9641e2520f63da0e8167474ea53ca4348471c696
ninaai517 <carolina.aizawa.moreira@gmail.com>
2023-11-06 11:09:58 -0300

    Configuracoes iniciais da aplicacao

---
+++ partida-futebol-api/src/main/java/br/com/meli/partidafutebolapi/PartidaFutebolApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.partidafutebolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidaFutebolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartidaFutebolApiApplication.class, args);
+	}
+
+}
---
+++ partida-futebol-api/src/test/java/br/com/meli/partidafutebolapi/PartidaFutebolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.partidafutebolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidaFutebolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
