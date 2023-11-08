2b3885b5c246175cf79a15f7dc8d6322a9f84667
ninaai517 <carolina.aizawa.moreira@gmail.com>
2023-11-08 12:30:10 -0300

    Criando a entidade Partida

---
+++ partida-futebol-api/src/main/java/br/com/meli/partidafutebolapi/model/Partida.java
@@ -0,0 +1,42 @@
+package br.com.meli.partidafutebolapi.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="tb_partida")
+@Getter
+@Setter
+@Data
+
+public class Partida {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_partida")
+    private Long id;
+
+    @Column(name = "clube_mandante")
+    private String clubeMandante;
+
+    @Column(name = "clube_visitante")
+    private String clubeVisitante;
+
+    @Column(name="resultado_mandante")
+    private int goalsMandante;
+
+    @Column(name="resultado_visitante")
+    private int goalsVisitante;
+
+    @Column(name="data_hora_partida")
+    private LocalDateTime dataHoraPartida;
+
+    @Column(name="estadio")
+    @NotBlank
+    private String nomeEstadio;
+}
---
+++ partida-futebol-api/src/main/java/br/com/meli/partidafutebolapi/repository/PartidaRepository.java
@@ -0,0 +1,9 @@
+package br.com.meli.partidafutebolapi.repository;
+
+import br.com.meli.partidafutebolapi.model.Partida;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartidaRepository extends JpaRepository<Partida,Long> {
+
+
+}
