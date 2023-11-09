65e86973f6cd357ec66a37a7dad5ad65812cabc2
ninaai517 <carolina.aizawa.moreira@gmail.com>
2023-11-09 10:30:50 -0300

    Refazendo a classe Model

---
+++ partida-futebol-api/src/main/java/br/com/meli/partidafutebolapi/controller/PartidaController.java
@@ -0,0 +1,11 @@
+package br.com.meli.partidafutebolapi.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/partida")
+public class PartidaController {
+
+
+
+}
---
+++ partida-futebol-api/src/main/java/br/com/meli/partidafutebolapi/repository/PartidaRepository.java
@@ -0,0 +1,7 @@
+package br.com.meli.partidafutebolapi.repository;
+
+import br.com.meli.partidafutebolapi.model.PartidaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartidaRepository extends JpaRepository<PartidaModel,Long> {
+}
